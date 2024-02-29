c4af4129092bfb9facc32b689ccee3121764a548
adamalbny <adam.albani.t@gmail.com>
2024-02-29 16:15:42 +0700

    Initial Commit

--- countCharacter.php
+++ countCharacter.php
@@ -3,13 +3,15 @@
 function countChar($char){
     $arrChar = array();
     for($i = 0; $i < strlen($char); $i++){
-        $count = 1;
-        for($z = 0; $z < strlen($char); $z++)
-        if($char[$z] == $char[$i]){
-            array_push($arrChar, array($char[$z] => $count++));
+        $currentChar = $char[$i];
+        if($arrChar[$currentChar] != $char[$i]){
+            $arrChar[$currentChar]++;
+        }else{
+            $arrChar[$currentChar] = 1;
         }
     }
     var_dump($arrChar);
 }   
-countChar('Testtest');
+
+countChar('Testes');
 ?>
